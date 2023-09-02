1625b6044922c99a0e707bee0871617c63976f46
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-09-03 01:00:39 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/Io']
+      external: ['react-icons/Io', 'react-icons/Ri']
     }
   }
 })
