94f9c5f7c36e9d6543bf375cf6eabecc0346e1df
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-09-04 03:05:11 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ import {RiInstagramFill} from "react-icons/ri"
 export default function Footer(){
     return(
         <>
-            <div className="py-5 bg-blue-400 text-blue-950 lg:px-32">
+            <div className="py-5 md:bg-blue-400 text-blue-950 lg:px-32">
                 <div className="flex justify-between items-center">
-                    <div className="text-3xl font-bold">Tasker</div>
+                    <div className="text-3xl font-semibold md:font-bold">Tasker</div>
                     <div className="font-semibold">Copyrights © 2023</div>
                     <div className="flex justify-between space-x-2  lg:space-x-4 text-xl">
                         <a href="https://www.linkedin.com/in/pradeep-pradeep-1bbb3521a/"><TiSocialLinkedin/></a>
